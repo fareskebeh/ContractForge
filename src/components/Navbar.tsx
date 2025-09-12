a1dfa9ce4c83219cffb9c102d27281df78a92704
fareskebeh <fares.kebbeh@gmail.com>
2025-09-12 15:10:08 +0300

    feat: enable visibility of the sidebar button only when you're in /chatbot

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,21 @@ import type React from "react"
 import { Github, Sidebar } from "lucide-react"
 import "./ui/Navbar.css"
 import { Link } from "react-router-dom";
+import {useLocation} from "react-router-dom"
 
 type Props = {
   setOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const Navbar = ({setOpen}: Props) => {
+  const {pathname} = useLocation()
+  
   return (
     <nav className="navbar">
       <div className="navbar-container">
         {/* Logo Section */}
         <div className="navbar-logo">
-          <button className="sidebar-button" onClick={()=>setOpen(true)}>
+          <button className={`${pathname!=="/chatbot" ? "hidden" : ""} sidebar-button`} onClick={()=>setOpen(true)}>
             <Sidebar/>
           </button>          
           <a href="https://resai.resilientdb.com" target="_blank" rel="noopener noreferrer">
